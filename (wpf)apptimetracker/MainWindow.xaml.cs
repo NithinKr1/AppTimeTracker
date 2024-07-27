c6068d05d38dda36cc2901efb24da9281d8e0ad6
Nithin Krishna <106225473+NithinKr1@users.noreply.github.com>
2024-07-27 23:37:22 +0200

    Finally added the WPF project to the solution but it still needs to fix The svelte and python are in expected state.

---
+++ (wpf)apptimetracker/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+using Microsoft.Web.WebView2.Core;
+
+namespace WebView2Example
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWebView();
+        }
+
+        async void InitializeWebView()
+        {
+            await webView.EnsureCoreWebView2Async(null);
+            webView.CoreWebView2.Navigate("http://localhost:5173");
+        }
+    }
+}
