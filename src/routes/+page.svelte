ade013ef03fd5dd48da4cb78cc023522f1b2a260
Nithin Krishna <106225473+NithinKr1@users.noreply.github.com>
2024-07-27 21:47:04 +0200

    1st Commit: Added new files (sveltekit) to the project for the first time.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import { onMount } from 'svelte';
+    let processData = {};
+  
+    async function fetchData() {
+      const response = await fetch('http://localhost:5000/data');
+      processData = await response.json();
+    }
+  
+    onMount(fetchData);
+  </script>
+  
+  <main>
+    <h1>Screen Activity Tracker</h1>
+    <ul>
+      {#each Object.entries(processData) as [key, value]}
+        <li>{key}: {value} seconds</li>
+      {/each}
+    </ul>
+  </main>
+  
