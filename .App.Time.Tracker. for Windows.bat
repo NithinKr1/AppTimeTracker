423287005d9507b228fd113c822568adef0bbc5f
Nithin Krishna <106225473+NithinKr1@users.noreply.github.com>
2024-07-31 15:33:57 +0200

    Removed npm dev and changed python->pythonw

--- .App.Time.Tracker. for Windows.bat
+++ .App.Time.Tracker. for Windows.bat
@@ -8,8 +8,4 @@ REM call path\to\your\venv\Scripts\activate.bat
 REM Start the backend script
 start pythonw apptracker_main.py --sleep_time 1 --port 5000
 
-REM Start the frontend script using npm
-start npm run dev
-
-REM Optional: Wait for both processes to end
 REM wait
