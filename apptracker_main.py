b4dc25146c7b19e64e9f8d80e4ffe390058cd24d
Nithin Krishna <106225473+NithinKr1@users.noreply.github.com>
2024-07-31 17:54:36 +0200

    This will not be changed and final now.

--- apptracker_main.py
+++ apptracker_main.py
@@ -1,6 +1,3 @@
-import os
-import signal
-import sys
 import time
 import psutil
 import re
@@ -12,7 +9,6 @@ import pygetwindow as gw
 import win32process
 import win32gui
 from titlecase import titlecase
-import pystray
 from pystray import MenuItem as item
 
 class ScreenActivityTracker:
@@ -30,6 +26,7 @@ class ScreenActivityTracker:
             return None
 
     def track_activity(self, sleep_time):
+        '''Track the active window and the time spent on it.'''
         while self.do_run:
             try:
                 current_app = psutil.Process(win32process.GetWindowThreadProcessId(win32gui.GetForegroundWindow())[1]).name().replace(".exe", " ")
@@ -53,10 +50,12 @@ class ScreenActivityTracker:
                 logging.error(f"Error tracking activity: {e}")
 
     def get_sorted_process_time(self):
+        '''Return the process time in descending order.'''
         sorted_process_time = {k: v for k, v in sorted(self.process_time.items(), key=lambda item: item[1], reverse=True)}
         return jsonify(sorted_process_time)
 
 def create_app(tracker):
+    '''Create a Flask app to serve the process time data.'''
     app = Flask(__name__)
 
     @app.route('/data', methods=['GET'])
@@ -65,22 +64,9 @@ def create_app(tracker):
 
     return app
 
-# def on_exit(icon, item):
-#     icon.stop()
-#     tracker.do_run = False
-
-# def setup_tray_icon():
-#     # Determine the directory of the current script
-#     script_dir = os.path.dirname(os.path.abspath(__file__))
-#     image_path = os.path.join(script_dir, "apptrackerlogo.ico")
-
-#     image = Image.open(image_path)
-#     menu = pystray.Menu(item('Quit', on_exit))
-#     icon = pystray.Icon("Screen Activity Tracker", image, "Screen Activity Tracker", menu)
-#     icon.run()
-
 if __name__ == "__main__":
-    import argparse
+    import argparse 
+    # Setting up sleep time and port number to the argument
     parser = argparse.ArgumentParser(description='Track screen activity.')
     parser.add_argument('--sleep_time', type=int, default=1, help='The time to sleep between checks for the active window.')
     parser.add_argument('--port', type=int, default=5000, help='The port to run the Flask app on.')
@@ -88,16 +74,12 @@ if __name__ == "__main__":
 
     logging.basicConfig(filename='screen_activity_tracker.log', level=logging.INFO)
 
+    # Create a ScreenActivityTracker instance 
     tracker = ScreenActivityTracker()
     tracking_thread = Thread(target=tracker.track_activity, args=(args.sleep_time,))
     tracking_thread.daemon = True
     tracking_thread.start()
 
     app = create_app(tracker)
-    # flask_thread = Thread(target=lambda: app.run(port=args.port))
-    # flask_thread.daemon = True
-    # flask_thread.start()
     app=create_app(tracker)
     app.run(port=args.port)
-
-    # setup_tray_icon()
